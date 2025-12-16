23e129f9e9fe302bb991c523b1b7cbd5db5c9d51
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-12-16 04:55:18 +0100

    Recipe chnages + Space age

---
+++ easy-wind-turbine/Code/RecipesSpaceAge.lua
@@ -0,0 +1,147 @@
+---@class LuaSettings
+local SS = settings.startup
+
+if mods["space-age"] then
+    data:extend({
+        {
+            type = "recipe",
+            name = "EasyWindTurbine1",
+            category = "electronics",
+            energy_required = 5,
+            ingredients = {
+                {type = "item", name = "electronic-circuit", amount = 5},
+                {type = "item", name = "stone-brick", amount = 20},
+                {type = "item", name = "steel-plate", amount = 10},
+                {type = "item", name = "engine-unit", amount = 5},
+            },
+            results = {{type="item", name= "EasyWindTurbine1", amount=1}},
+            enabled = false
+        },
+    })
+    if SS["EasyWindTurbine"].value == "Crafting" then
+        data:extend({
+            {
+                type = "recipe",
+                name = "EasyWindTurbine2",
+                category = "electronics",
+                energy_required = 10,
+                ingredients = {
+                    {type = "item", name = "advanced-circuit", amount = 5},
+                    {type = "item", name = "electric-engine-unit", amount = 5},
+                    {type = "item", name = "refined-concrete", amount = 20},
+                    {type = "item", name = "carbon", amount = 20},
+                    {type = "item", name = "battery", amount = 10},
+                },
+                results = {{type="item", name= "EasyWindTurbine2", amount=1}},
+                enabled = false
+            },
+            {
+                type = "recipe",
+                name = "EasyWindTurbine3",
+                category = "electronics",
+                energy_required = 10,
+                ingredients = {
+                    {type = "item", name = "low-density-structures", amount = 10},
+                    {type = "item", name = "tungsten-carbide", amount = 50},
+                    {type = "item", name = "EasyWindTurbine2", amount = 1},
+                    {type = "item", name = "processing-unit", amount = 5},
+                    {type = "item", name = "superconductor", amount = 30},
+                    {type = "item", name = "carbon-fiber", amount = 50},
+                },
+                results = {{type="item", name= "EasyWindTurbine3", amount=1}},
+                enabled = false
+            },--[[
+            {
+                type = "recipe",
+                name = "EasyWindTurbine4",
+                energy_required = 10,
+                ingredients = {
+                    {type = "item", name = "electric-engine-unit", amount = 5},
+                    {type = "item", name = "advanced-circuit", amount = 5},
+                    {type = "item", name = "plastic-bar", amount = 20},
+                    {type = "item", name = "battery", amount = 10},
+                },
+                results = {{type="item", name= "EasyWindTurbine2", amount=1}},
+                enabled = false
+            },
+            {
+                type = "recipe",
+                name = "EasyWindTurbine5",
+                energy_required = 10,
+                ingredients = {
+                    {type = "item", name = "low-density-structures", amount = 10},
+                    {type = "item", name = "tungsten-carbide", amount = 50},
+                    {type = "item", name = "EasyWindTurbine2", amount = 1},
+                    {type = "item", name = "processing-unit", amount = 5},
+                    {type = "item", name = "superconductor", amount = 30},
+                    {type = "item", name = "carbon-fiber", amount = 50},
+                },
+                results = {{type="item", name= "EasyWindTurbine3", amount=1}},
+                enabled = false
+            },]]
+        })
+    end
+    if SS["EasyWindTurbine"].value == "Upgrading" then
+        data:extend({
+            {
+                type = "recipe",
+                name = "EasyWindTurbine2",
+                category = "electronics",
+                energy_required = 10,
+                ingredients = {
+                    {type = "item", name = "EasyWindTurbine1", amount = 1},
+                    {type = "item", name = "electric-engine-unit", amount = 10},
+                    {type = "item", name = "refined-concrete", amount = 20},
+                    {type = "item", name = "carbon", amount = 20},
+                    {type = "item", name = "battery", amount = 10},
+                },
+                results = {{type="item", name= "EasyWindTurbine2", amount=1}},
+                enabled = false
+            },
+            {
+                type = "recipe",
+                name = "EasyWindTurbine3",
+                category = "electronics",
+                energy_required = 10,
+                ingredients = {
+                    {type = "item", name = "low-density-structures", amount = 10},
+                    {type = "item", name = "tungsten-carbide", amount = 50},
+                    {type = "item", name = "EasyWindTurbine2", amount = 1},
+                    {type = "item", name = "processing-unit", amount = 5},
+                    {type = "item", name = "superconductor", amount = 30},
+                    {type = "item", name = "carbon-fiber", amount = 50},
+                },
+                results = {{type="item", name= "EasyWindTurbine3", amount=1}},
+                enabled = false
+            },--[[
+            {
+                type = "recipe",
+                name = "EasyWindTurbine4",
+                energy_required = 10,
+                ingredients = {
+                    {type = "item", name = "electric-engine-unit", amount = 5},
+                    {type = "item", name = "advanced-circuit", amount = 5},
+                    {type = "item", name = "plastic-bar", amount = 20},
+                    {type = "item", name = "battery", amount = 10},
+                },
+                results = {{type="item", name= "EasyWindTurbine2", amount=1}},
+                enabled = false
+            },
+            {
+                type = "recipe",
+                name = "EasyWindTurbine5",
+                energy_required = 10,
+                ingredients = {
+                    {type = "item", name = "low-density-structures", amount = 10},
+                    {type = "item", name = "tungsten-carbide", amount = 50},
+                    {type = "item", name = "EasyWindTurbine2", amount = 1},
+                    {type = "item", name = "processing-unit", amount = 5},
+                    {type = "item", name = "superconductor", amount = 30},
+                    {type = "item", name = "carbon-fiber", amount = 50},
+                },
+                results = {{type="item", name= "EasyWindTurbine3", amount=1}},
+                enabled = false
+            },]]
+        })
+    end
+end
